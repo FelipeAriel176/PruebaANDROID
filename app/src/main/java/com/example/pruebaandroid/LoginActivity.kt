513247e9415eb88c834694e294d3dfff19d567bf
Felipe Martinez <martinezfelipe176@gmail.com>
2026-09-07 15:05:38 -0300

    formulario registro listo

--- app/src/main/java/com/example/pruebaandroid/LoginActivity.kt
+++ app/src/main/java/com/example/pruebaandroid/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.pruebaandroid
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,7 +8,8 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.Button
 import android.content.Intent
-
+import android.widget.EditText
+import android.widget.Toast
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,17 +20,29 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
+        val inputUsuarioLogin = findViewById<EditText>(R.id.editTextText2)
+        val inputPassLogin = findViewById<EditText>(R.id.editTextTextPassword)
         val botonIngresar = findViewById<Button>(R.id.btnIngresar)
         val botonRegistro = findViewById<Button>(R.id.btnCrear)
+
         botonRegistro.setOnClickListener {
             val intentRegistro = Intent(this, RegistroActivity::class.java)
             startActivity(intentRegistro)
         }
-        botonIngresar.setOnClickListener {
-            val intentBienvenida = Intent(this, Bienvenida::class.java)
-            startActivity(intentBienvenida)
-        }
 
+        botonIngresar.setOnClickListener {
+            val preferencias = getSharedPreferences("DatosUsuario", Context.MODE_PRIVATE)
+            val usuarioGuardado = preferencias.getString("Usuario", "")
+            val passGuardada = preferencias.getString("Contrasena", "")
+            val usuarioEscrito = inputUsuarioLogin.text.toString()
+            val passEscrita = inputPassLogin.text.toString()
+            if (usuarioEscrito == usuarioGuardado && passEscrita == passGuardada) {
+                val intentBienvenida = Intent(this, Bienvenida::class.java)
+                startActivity(intentBienvenida)
+
+            } else {
+                Toast.makeText(this, "Datos incorrectos", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/pruebaandroid/RegistroActivity.kt
+++ app/src/main/java/com/example/pruebaandroid/RegistroActivity.kt
@@ -8,7 +8,10 @@ import android.content.Context
 import android.content.Intent
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.core.view.WindowInsetsCompat
+
 class RegistroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +23,15 @@ class RegistroActivity : AppCompatActivity() {
             insets
         }
         val campoNombre = findViewById<EditText>(R.id.inputNombre1)
+        val campoApellido = findViewById<EditText>(R.id.apellido1)
+        val campoDireccion = findViewById<EditText>(R.id.editTextText4)
+        val campoRUT = findViewById<EditText>(R.id.editTextText5)
+        val campoEdad = findViewById<EditText>(R.id.editTextNumber)
+        val campoTelefono = findViewById<EditText>(R.id.editTextPhone)
+        val campoCorreo = findViewById<EditText>(R.id.correo)
+        val campoPass = findViewById<EditText>(R.id.passwrd)
         val btnValidar = findViewById<Button>(R.id.btnCrearr)
+        val grupoGenero = findViewById<RadioGroup>(R.id.generoID)
 
         btnValidar.setOnClickListener {
 
@@ -28,6 +39,21 @@ class RegistroActivity : AppCompatActivity() {
             val editor = preferencias.edit()
             val nombreIngresado = campoNombre.text.toString()
             editor.putString("Nombre", nombreIngresado)
+            editor.putString("Usuario", campoCorreo.text.toString())
+            editor.putString("Contrasena", campoPass.text.toString())
+            editor.putString("Apellido", campoApellido.text.toString())
+            editor.putString("Dirección", campoDireccion.text.toString())
+            editor.putString("Rut", campoRUT.text.toString())
+            editor.putString("Edad", campoEdad.text.toString())
+            editor.putString("Telefono", campoTelefono.text.toString())
+
+            val idSeleccionado = grupoGenero.checkedRadioButtonId
+            if (idSeleccionado != -1) {
+                val radioSeleccionado = findViewById<RadioButton>(idSeleccionado)
+                editor.putString("Genero", radioSeleccionado.text.toString())
+            } else {
+                editor.putString("Genero", "No seleccionado")
+            }
             editor.apply()
             val intentVolver = Intent(this, LoginActivity::class.java)
             startActivity(intentVolver)
